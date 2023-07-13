e7cb3c8e5814078ec60eb7572f4af70d3d708d2f
MariaGlushtsova <glushtsova@tut.by>
2023-07-14 00:01:09 +0300

    Create UICollectionView

---
+++ IOS10-HW14-Maria Glushtsova/Sources/Cell/AlbumCell.swift
@@ -0,0 +1,21 @@
+//
+//  AlbumCell.swift
+//  IOS10-HW14-Maria Glushtsova
+//
+//  Created by Admin on 13.07.23.
+//
+
+import UIKit
+
+final class AlbumCell: UICollectionViewCell {
+    
+    static let identifier = "AlbumCell" 
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
